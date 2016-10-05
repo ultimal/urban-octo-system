d50bc0388251f2e1aa761f8bd5fed399dcbccafc
Haider Raza <haider_raza@Haiders-MacBook-Air.local>
2016-10-05 19:08:26 -0400

    Initial Commit

---
+++ Blocked/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Blocked
+//
+//  Created by Haider Raza on 2016-10-05.
+//  Copyright (c) 2016 Haider Raza. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
